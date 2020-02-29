e7e99102de75a793cd317c861ab0094423738a2d
jui patel <amin.jui2712@gmail.com>
2020-02-29 17:40:48 -0500

    mysql connection

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ var connection = mysql.createConnection({
   password: "Bik1984att@",
   database: "employees"
 }); 
+
+var showroles;
+var showdepartments;
+var showemployees;
+
+// Initiate MySQL Connection.
+connection.connect(function (err) {
+  
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+
+  connection.query("SELECT * from role", function (error, res) {
+    showroles = res.map(role => ({ name: role.title, value: role.id }))
+  })
+  connection.query("SELECT * from department", function (error, res) {
+    showdepartments = res.map(dep => ({ name: dep.name, value: dep.id }))
+  })
+  connection.query("SELECT * from employee", function (error, res) {
+    // console.log(error, res);
+    showemployees = res.map(emp => ({ name: `${emp.first_name} ${emp.last_name}`, value: emp.id }))
+  })
+
+  showmenu();
+})
