7a23671c9a8ff863f820932e40c18bfb6758a83c
jui patel <amin.jui2712@gmail.com>
2020-02-29 17:33:36 -0500

    mysql DB connection information

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 var mysql = require("mysql");
 const inquirer = require("inquirer");
 const confirm = require('inquirer-confirm');
+
+// MySQL DB Connection Information
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Bik1984att@",
+  database: "employees"
+}); 
