87b1a2cc809ea8132b571832d007bd6036621fdb
jui patel <amin.jui2712@gmail.com>
2020-03-03 11:40:14 -0500

    final commit

--- index.js
+++ index.js
@@ -244,3 +244,49 @@ function addEmployeeRole(data) {
   });
   endOrMenu();
 }
+
+function updateRole() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "For which employee would you like to update the role?",
+        name: "empID",
+        choices: showemployees
+      },
+      {
+        type: "list",
+        message: "What is the employee's new role?",
+        name: "titleID",
+        choices: showroles
+      }
+    ])
+    .then(function (response) {
+      // console.log(response);
+      updateEmployeeRole(response);
+    })
+}
+
+function updateEmployeeRole(data) {
+  connection.query(`UPDATE employee SET role_id = ${data.titleID} WHERE id = ${data.empID}`,
+  function (error, res) {
+    // console.log(error, res);
+    if (error) throw error;
+  });
+  endOrMenu();
+}
+
+function endOrMenu() {
+  confirm("Would you like to continue?")
+  .then(function confirmed() {
+    showmenu();
+  }, function cancelled() {
+    end();
+  });
+}
+
+function end() {
+  console.log("Thank you for using Employee Tracker!");
+  connection.end();
+  process.exit();
+}
